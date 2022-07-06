750a567d81b87742a34e14bd35dd7a2c326059dc
JASantia85 <JuanASantiago1058@gmail.com>
2022-07-06 17:28:09 -0400

    creates score component

---
+++ src/Score.jsx
@@ -0,0 +1,7 @@
+const Scores = (props) => {
+  return ( 
+    <h2>This is a score component</h2>
+  );
+}
+
+export default Scores;
--- src/StudentCard.jsx
+++ src/StudentCard.jsx
@@ -1,8 +1,11 @@
+import Score from "./Score";
+
 const StudentCard = ({student}) => {
   return ( 
     <div className="student-card">
       <h2>This is {student.name}</h2>
       <h3>{student.bio}</h3>
+      <Score />
     </div>
   );
 }
